3db1bbb6fb23aea715ca314c42239f9e734bc173
bryan robalino <eduardochuez_2003@outlook.com>
2023-07-02 14:34:58 -0500

    agregue el api y todo lo que faltaba

--- app/src/main/java/xyz/cumaki/apirestfulvolley/MainActivity2.java
+++ app/src/main/java/xyz/cumaki/apirestfulvolley/MainActivity2.java
@@ -4,9 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
@@ -24,29 +21,23 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity{
-
-
+public class MainActivity2 extends AppCompatActivity {
+    public String Token = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btEnviar=findViewById(R.id.Enviar);
-        btEnviar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                API();
-            }
-        });
+        setContentView(R.layout.activity_main2);
+        Token=getIntent().getStringExtra("Token");
 
 
+        API();
     }
 
+
     public void  API() {
-        TextView Txt = (TextView) findViewById(R.id.textView3);
+        TextView txt = (TextView) findViewById(R.id.textView2);
         RequestQueue queue = Volley.newRequestQueue(this);
-        Intent i = new Intent(this, MainActivity2.class);
-        String url ="https://api.uealecpeterson.net/public/login";
+        String url ="https://api.uealecpeterson.net/public/productos/search";
 
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
@@ -54,10 +45,17 @@ public class MainActivity extends AppCompatActivity{
                     public void onResponse(String response) {
 
                         try {
+                            String Productos="";
                             JSONObject resp = new JSONObject(response);
-                            String Token = resp.get("access_token").toString();
-                            i.putExtra("Token",Token);
-                            startActivity(i);
+                            JSONArray users = resp.getJSONArray("productos");
+                            for(int i=0; i<users.length();i++){
+                                JSONObject descripcion = users.getJSONObject(i);
+                                Productos = Productos+ "\n" + descripcion.getString("barcode").toString()
+                                        +", " +descripcion.getString("descripcion").toString()
+                                        + ", "+ descripcion.getString("precio_unidad").toString();
+                            }
+                            txt.setText(Productos);
+
 
                         } catch (JSONException e) {
                             throw new RuntimeException(e);
@@ -68,24 +66,20 @@ public class MainActivity extends AppCompatActivity{
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Txt.setText("Error en la solicitud: " + error.toString());
+                        txt.setText(error.toString());
                     }
                 }){
-            @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
                 Map<String, String> headerMap = new HashMap<String, String>();
+                headerMap.put("Authorization", "Bearer " + Token);
                 return headerMap;
             }
             protected Map<String, String> getParams(){
                 Map<String, String> params = new HashMap<String, String>();
-                EditText Correo = findViewById(R.id.txtCorreo);
-                EditText Clave = findViewById(R.id.txtContraseña);
-                params.put("correo", Correo.getText().toString());
-                params.put("clave", Clave.getText().toString());
+                params.put("fuente","1");
                 return params;
             }
         };
         queue.add(stringRequest);
     }
-
 }
